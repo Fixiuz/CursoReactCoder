91ffa092fc13bc16b7f241fd8c7839983c8393f8
Fixiuz <139408339+Fixiuz@users.noreply.github.com>
2025-03-12 11:12:02 -0300

    StyleCommit1

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,8 @@ function App() {
   return (
     <>
       <NavBar/>
-      <ItemListConteiner greetings="Mensaje de bienvenida"/>
-
+      <ItemListConteiner greetings="¡Welcome to our store!"/>
+      
       
       
     </>
